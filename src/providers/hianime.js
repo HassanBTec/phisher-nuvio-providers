1310528214c8b72b6b343c951219e08e00eee147
Phisher98 <mandiktest2@gmail.com>
2026-02-27 19:10:45 +0530

    Fix

--- src/providers/hianime.js
+++ src/providers/hianime.js
@@ -32,9 +32,8 @@ function extractMegacloud(embedUrl, effectiveType) {
     const mainUrl = 'https://megacloud.blog';
 
     const headers = { 'Accept': '*/*', 
-        'X-Requested-With': 'XMLHttpRequest',
         'Referer': mainUrl, 
-        'User-Agent': 'Mozilla/5.0' 
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0' 
     };
 
 
